3bbb1ca25949d33e288f9e1dcb650d1a01fcc5de
Up789486 <up789486@myport.ac.uk>
2017-02-17 21:17:05 +0000

    Add files via upload

---
+++ createBillConfirm.php
@@ -0,0 +1,71 @@
+<?php
+
+	$total = $_POST['totalCost'];
+	$name = $_POST['billName'];
+	$date = $_POST['dueDate'];
+	$payers = $_POST['payers'];
+	$percent = $_POST['percentage'];
+	
+	$x = ($total / 100) * $percent;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Create Bill Confirmation</title>
+ 
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+      <link href="css/maincss.css" rel="stylesheet">
+ 
+   
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container">
+    <div class="row login">
+        <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 well">
+            <form role="form">
+              <div class="form-group text-center">
+                <div class="logo">
+                    <p>Open Bill</p>
+					<p>You have successfully created a new bill <p>
+                    <p>Your new bill: <?php echo $name; ?> is a due on the <?php echo $date; ?></p>
+                    <p>Spilt between <?php echo $payers; ?> people at a percentage of <?php echo $percent; ?>% each </p>
+                    <p>This comes to an amount of £<?php echo $x; ?> per person. </p>
+					
+                </div>
+				<div>
+					
+              </div>
+			  <div class="form-group">
+                <a class="btn btn-default btn-lg btn-block btn-success"href="#">Pay Bill Now</a>
+              </div>
+			  <div class="form-group">
+                <a class="btn btn-default btn-lg btn-block btn-success"href="savebill.html">Pay Bill Later</a>
+              </div>
+			  <div class="form-group">
+                <a class="btn btn-default btn-lg btn-block btn-success"href="emailbill.html">Email Bill</a>
+              </div>
+			  <div class="form-group last-row">
+                <a href="menu.html" class="pull-right">Menu</a>
+				<a href="index.html" class="pull-left">Sign Out</a>
+              </div>
+              
+            </form>
+        </div>
+    </div>
+</div>
+ 
+   
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
+
