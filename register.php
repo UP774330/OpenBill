120f19caa2d63d7c0fc8fff8bb10ebc2a2114d36
UP774330 <UP774330@myport.ac.uk>
2017-02-17 02:56:17 +0000

    updated php

--- register.php
+++ register.php
@@ -1,66 +1,87 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
+<?php
+	require('connect.php');
+    // If the values are posted, insert them into the database.
+    if (isset($_POST['username']) && isset($_POST['password'])){
+        $username = $_POST['username'];
+        $firstname = $_POST['email'];
+        $lastname = $_POST['firstname'];
+	      $email = $_POST['lastname'];
+        $password = $_POST['password'];
 
-    <!-- Bootstrap -->
-		<!-- Latest compiled and minified CSS -->
+        $query = "INSERT INTO `login` (username, firstname, lastname, password, email) VALUES ('$username', '$firstname', '$lastname', '$email', '$password')";
+        $result = mysqli_query($connection, $query);
+        if($result){
+            $smsg = "User Created Successfully.";
+        }else{
+            $fmsg ="User Registration Failed";
+        }
+    }
+    ?>
+<html>
+<head>
+  <meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Register</title>
+
+<!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
 
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
 
-<link rel="stylesheet" href="css/maincss.css">
+<link rel="stylesheet" href="maincss.css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <div class="container">
-    <div class="row login">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <div class="row login">
+      <form class="form-signin" method="POST">
         <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 well">
-            <form role="form">
-              <div class="form-group text-center">
-                <div class="logo">
-                    <p>Open Bill</p>
-                </div>
+          <div role="form">
+            <div class="form-group text-center">
+              <div class="logo">
+                <p>Open Bill</p>
               </div>
-              <div class="form-group">
-                <input type="text" class="form-control input-lg" id="userid" placeholder="Username">
-              </div>
-			  <div class="form-group">
-                <input type="firstname" class="form-control input-lg" id="firstName" placeholder="First Name">
-              </div>
-			  <div class="form-group">
-                <input type="last name" class="form-control input-lg" id="lastname" placeholder="Last Name">
-              </div>
-              <div class="form-group">
-                <input type="emailaddress" class="form-control input-lg" id="emailAddress" placeholder="Email Address">
-              </div>
-			  <div class="form-group">
-                <input type="password" class="form-control input-lg" id="password" placeholder="Password">
-              </div>
-              <div class="form-group">
-                <button type="submit" class="btn btn-default btn-lg btn-block btn-success">Submit</button>
-              </div>
-              <div class="form-group last-row">
-                <a href="index.html" class="pull-right">Sign In</a>
-              </div>
-            </form>
+
+      <?php if(isset($smsg)){ ?><div class="alert alert-success" role="alert"> <?php echo $smsg; ?> </div><?php } ?>
+      <?php if(isset($fmsg)){ ?><div class="alert alert-danger" role="alert"> <?php echo $fmsg; ?> </div><?php } ?>
+      <div class "form-group">
+	      <input type="text" name="username" class="form-control input-lg" placeholder="Username" required>
+      </div>
+      <div class "form-group">
+        <input type="text" name="firstname" id="inputFirstName" class="form-control input-lg" placeholder="First Name" required>
+      </div>
+      <div class "form-group">
+        <input type="text" name="lastname" id="inputLastName" class="form-control input-lg" placeholder="Last Name" required>
         </div>
+        <div class "form-group">
+        <input type="email" name="email" id="inputEmail" class="form-control input-lg" placeholder="Email address" required autofocus>
+        </div>
+        <div class "form-group">
+        <input type="password" name="password" id="inputPassword" class="form-control input-lg" placeholder="Password" required>
+        </div>
+        <div class "form-group">
+        <button class="btn btn-default btn-lg btn-block btn-success" type="submit">Register</button>
+        </div>
+        <div class "form-group">
+        <a class="btn btn-default btn-lg btn-block btn-success" href="index.html">Login</a>
+        </div>
+      </form>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-  </body>
+
+</body>
+
 </html>
